abfd641d62e294bee315efac4efea9e3a1359986
Akshay <akshay@cloudrevelinnovation.com>
2024-08-13 15:36:11 +0530

    mobile responsive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { SuggestionsComponent } from "../suggestions/suggestions.component";
 import { StoryComponent } from "../story/story.component";
 import { PostsComponent } from "../posts/posts.component";
 import { BottomNavigationComponent } from "../bottom-navigation/bottom-navigation.component";
+import { HeaderMobileComponent } from "../header-mobile/header-mobile.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SuggestionsComponent, StoryComponent, PostsComponent, BottomNavigationComponent],
+  imports: [SuggestionsComponent, StoryComponent, PostsComponent, BottomNavigationComponent, HeaderMobileComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
