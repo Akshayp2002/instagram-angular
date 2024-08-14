96b5c6813c24753040664337b0d4a1015744fbd0
Akshay <akshay@cloudrevelinnovation.com>
2024-08-14 16:13:24 +0530

    profile page

---
+++ src/app/core-components/user-posts/user-posts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-posts',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-posts.component.html',
+  styleUrl: './user-posts.component.css'
+})
+export class UserPostsComponent {
+
+}
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MessageHeadderComponent } from "../core-components/message-headder/message-headder.component";
+import { UserPostsComponent } from "../core-components/user-posts/user-posts.component";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [MessageHeadderComponent],
+  imports: [MessageHeadderComponent, UserPostsComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
